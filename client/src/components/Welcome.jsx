00be539777a0a37f32926038e550889276be65bf
kartikchoubisa <f20190896@goa.bits-pilani.ac.in>
2022-01-26 22:42:36 +0530

    navbar completed

---
+++ client/src/components/Welcome.jsx
@@ -0,0 +1,10 @@
+const Welcome = () => {
+    return (
+        <div className="welcome">
+            <h1>Welcome</h1>
+            <input type="name" name="enter name"/>
+            </div>
+    )
+}
+
+export default Welcome;
